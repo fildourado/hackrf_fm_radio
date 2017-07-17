still unable to find the FM modulated broadcast i'm looking for. gain parameters are likely incorrect. amp was disabled during data collection. need to sample at lower rate, FFTs taking too long and too wide spectrum

python/plot_rx_data.py
@@ -5,7 +5,16 @@ import math
 from struct import pack, unpack
 import cmath
 from scipy.fftpack import fft, fftfreq, fftshift
+from scipy import signal
 
+def lpf_filter_coeff(order, fs, cutoff):
+
+    nyq = fs/2
+    normal_cutoff = cutoff/nyq
+
+    b,a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+
+    return b,a
 
 def main():
 
@@ -15,66 +24,115 @@ def main():
     filepath = path+filename
 
     fs = 10e6 # samples/sec
-    x = []
+    fc = 107950000.0 # center frequency
 
-    N = 2**22 # number of samples to collect
+
+    #N = 2**22 # number of samples to collect
     #N = 2**21 # number of samples to collect
+    N = 2**16 # FFT window chunk
+    N_buffers = 1000
+
+    x = np.zeros(N) + 1j*np.zeros(N)
+    print x
+    signal_buffer = []
+
     i = 0
+    buffer_cnt = 0
     with open(filepath) as f:
         while True:
-            c = f.read(1)
-            if not c:
+
+            I = f.read(1)
+            Q = f.read(1)
+
+            if not I:
                 print "End of file"
+                N_buffers = buffer_cnt
                 break
 
-            x.append(1.0*ord(c))
-
-            #unsigned = ord(c)
-            #signed = unsigned - 256 if unsigned > 127 else unsigned
-            #x.append(1.0*signed)
-
-            if i > N-2:
+            if not Q:
+                print "End of file"
+                N_buffers = buffer_cnt
                 break
 
-            i+=1
 
-    N = N/2
-    x = np.array(x)
+            if i < N:
+                temp_samp = 1.0*ord(I) + 1j*1.0*ord(Q)
+                x[i] = temp_samp
+                i += 1
+            else:
 
-    I = x[0::2]
-    Q = x[1::2]
-    signal = I + 1j * Q
+                if buffer_cnt == N_buffers:
+                    break
+
+                print buffer_cnt
+                signal_buffer.append( x.copy() )
+                buffer_cnt += 1
+                i = 0
+                #x.fill(0.0 + 1j*0.0)
 
 
-    t = np.array(range(N)) * (1.0/fs)
-    mag = abs(signal)
-    phase = np.angle(signal, deg=True)
+    #plt.figure(1)
+    #x = np.array(range(N))
+    #plt.plot(x, abs(signal[0]), 'r', label='0')
+    #plt.plot(x, abs(signal[1]), 'b', label='1')
+    #plt.show()
+    #ys.exit()
 
-    # Two subplots, the axes array is 1-d
-    f, axarr = plt.subplots(2, sharex=True)
-    axarr[0].plot(t, 10*np.log10(mag))
-    axarr[0].set_title('Sharing X axis')
-    axarr[1].plot(t, phase)
-
+    order = 6
+    cutoff = 200e3
+    b,a = lpf_filter_coeff(order, fs, cutoff)
 
     FFT_bins = N
     F_res = fs / FFT_bins
 
     print "# FFT Bins %f" % (FFT_bins)
     print "Freq resolution %f Hz/bin" % (F_res)
+    print "Max Frequency: %f MHz" % ( (fs/2.0)/1e6 )
 
-
-
-    yf = fft(signal)
-    xf = fftfreq(N, 1/fs)
+    print len(signal_buffer)
+    print N_buffers
+    """ Perform N_buffers FFTs and hold the peak value from each """
+    xf = fftfreq(N, 1.0 / fs)
     xf = fftshift(xf)
-    yplot = fftshift(yf)
+
+    xf = (xf + fc) / 1e6
+    #xf = (xf + fc)
+
+    peak_hold = np.zeros(N) - 1.0
+    avg = np.zeros(N)
+    #data = np.array([])
+
+    for i in range(N_buffers):
+
+        y = signal.lfilter(b, a, signal_buffer[i])
+
+        yf = fft(y)
+        yf = fftshift(yf)
+        yf = np.abs(yf)
+        temp = yf.copy()
+        peak_hold = np.maximum(peak_hold, temp)
+        avg = avg + (yf)
+        #data = np.concatenate((data,y))
+
+    avg = avg * (1.0/N_buffers)
+
+    plt.figure(1)
+    plt.plot(xf, 10 * np.log10( (1.0 / N) * peak_hold), 'b', label="Peak Hold" )
+    plt.plot(xf, 10 * np.log10( (1.0 / N) * avg), 'r--', label="Average" )
+    #plt.plot(xf, (1.0 / N) * peak_hold, 'b', label="Peak Hold" )
+    #plt.plot(xf, (1.0 / N) * avg, 'r--', label="Average" )
+    plt.xlim([107.9, 108.0])
+    plt.legend()
+    plt.grid()
 
     plt.figure(2)
-    #plt.plot(xf,yplot)
-    plt.plot( xf, 10*np.log10( (1.0 / N) * np.abs(yplot) ) )
-    #plt.xlim([-1,100])
-    plt.grid()
+    data = np.concatenate((signal_buffer[0],signal_buffer[100]))
+    f, t, Sxx = signal.spectrogram(data, fs)
+
+    f = (f + fc)/1e6
+    plt.pcolormesh(t, f, Sxx)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
     plt.show()
 
     sys.exit()